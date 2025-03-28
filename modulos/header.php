d754916ebdce717c2242de6aeb76a6463d89a2e8
Jbonett <jbn19860220@gmail.com>
2025-03-28 07:55:37 -0500

    first commit

---
+++ modulos/header.php
@@ -0,0 +1,15 @@
+<header id="header" class="header d-flex align-items-center sticky-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+
+      <a href="index.html" class="logo d-flex align-items-center me-auto">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1 class="sitename">Multi</h1>
+      </a>
+
+      <?php include_once("./modulos/nav.php") ?>
+
+      <a class="btn-getstarted" href="#about">Get Started</a>
+
+    </div>
+  </header>
